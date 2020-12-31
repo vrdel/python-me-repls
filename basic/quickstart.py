cbde15b84a14c71af8d0b029b22f840612b5645f
Daniel Vrcic <daniel.vrcic@gmail.com>
2020-12-31 14:41:23 +0100

    run blocking func and coroutine concurrently

--- basic/quickstart.py
+++ basic/quickstart.py
@@ -4,14 +4,21 @@ import asyncio
 import time
 
 
-async def main():
+async def coro1():
     print(f'{time.ctime()} Hello')
-    await asyncio.sleep(2.0)
+    await asyncio.sleep(10)
     print(f'{time.ctime()} Goodbye')
 
 
-loop = asyncio.get_event_loop()
-try:
-    loop.run_until_complete(main())
-finally:
-    loop.close()
+def blocking():
+    time.sleep(5.0)
+    print(f"{time.ctime()} Hello from a thread")
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_in_executor(None, blocking)
+        loop.run_until_complete(coro1())
+    finally:
+        loop.close()

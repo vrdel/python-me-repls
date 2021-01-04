76c49ce2d1a58f5df0d79bc13db917b599aaa48b
Daniel Vrcic <daniel.vrcic@gmail.com>
2021-01-04 10:33:07 +0100

    send() and await does not work

--- basic/coro1.py
+++ basic/coro1.py
@@ -9,6 +9,20 @@ async def coro1():
     return '123'
 
 
+async def coro2():
+    # try remove
+    await asyncio.sleep(1.0)
+    return '456'
+
+
+async def coro2_call():
+    print("coro2_called")
+    await asyncio.sleep(1.0)
+    result = await coro2()
+    print('The answer 3 is {}'.format(result))
+    return result
+
+
 if __name__ == '__main__':
     try:
         coro = coro1()
@@ -20,5 +34,21 @@ if __name__ == '__main__':
         coro.send(None)
 
     except StopIteration as e:
-        print('The answer is {}'.format(e.value))
+        print('The answer 1 is {}'.format(e.value))
         print(type(e))
+
+    try:
+        coro = coro2()
+        coro.send(None)
+
+    except StopIteration as e:
+        # never see this one. it can be seen only if asyncio.sleep() is removed
+        # in coro2()
+        print('The answer 2 is {}'.format(e.value))
+        print(type(e))
+
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(coro2_call())
+    finally:
+        loop.close()

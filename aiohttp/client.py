e9df6f8589229d17b35ad62a4ab299441c460c64
Daniel Vrcic <daniel.vrcic@gmail.com>
2021-01-05 15:46:26 +0100

    concurrent requests

--- aiohttp/client.py
+++ aiohttp/client.py
@@ -2,18 +2,28 @@
 
 import aiohttp
 import asyncio
+import time
 
 
-async def aiohttp_get():
+url1 = 'http://httpbin.org/get'
+url2 = 'https://api.randomuser.me'
+
+
+async def aiohttp_get(url):
     async with aiohttp.ClientSession() as session:
-        async with session.get('http://httpbin.org/get') as resp:
-            print(resp.status)
+        async with session.get(url) as resp:
+            print(resp.status, f'{time.ctime()}')
             print(await resp.text())
 
 
 if __name__ == '__main__':
     try:
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(aiohttp_get())
+        loop.run_until_complete(asyncio.gather(
+            aiohttp_get(url1),
+            aiohttp_get(url1),
+            aiohttp_get(url2),
+            aiohttp_get(url2)
+        ))
     finally:
         loop.close()

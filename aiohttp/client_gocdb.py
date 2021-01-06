3ec6023fa11d690854e1b680d758af306d4e231b
Daniel Vrcic <daniel.vrcic@gmail.com>
2021-01-06 17:27:22 +0100

    ensure SSL client and server verification

--- aiohttp/client_gocdb.py
+++ aiohttp/client_gocdb.py
@@ -3,6 +3,7 @@
 import aiohttp
 import asyncio
 import time
+import ssl
 
 
 SERVENDPI = 'https://goc.egi.eu/gocdbpi/private/?method=get_service_endpoint&scope=&next_cursor=0'
@@ -11,8 +12,11 @@ SERVGROUPPI = 'https://goc.egi.eu/gocdbpi/private/?method=get_service_group&scop
 
 
 async def aiohttp_get(url):
+    sslcontext = ssl.create_default_context(capath='/etc/grid-security/certificates/')
+    sslcontext.load_cert_chain('hostcert.pem', 'hostkey.pem')
+
     async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
+        async with session.get(url, ssl=sslcontext) as resp:
             print('HTTP ', resp.status, f' - {time.ctime()}')
             content = await resp.text()
             return content

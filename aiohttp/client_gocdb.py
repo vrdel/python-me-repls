73a351e55f06a509ed733508224c5a5fceab9ca3
Daniel Vrcic <daniel.vrcic@gmail.com>
2021-01-07 11:38:43 +0100

    connection and HTTP retries

--- aiohttp/client_gocdb.py
+++ aiohttp/client_gocdb.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
 
 import aiohttp
+import aiohttp.client_exceptions
 import asyncio
 import time
 import ssl
 import uvloop
 import xml.dom.minidom
+from aiohttp_retry import RetryClient, ExponentialRetry, ListRetry
 
+import logging
+import logging.handlers
+
+log = logging.getLogger('aiohttp_retry')
+log.setLevel(logging.DEBUG)
+log.addHandler(logging.handlers.SysLogHandler('/dev/log', logging.handlers.SysLogHandler.LOG_USER))
+log.addHandler(logging.StreamHandler())
 
 # paginated API
 # SERVENDPI = 'https://goc.egi.eu/gocdbpi/private/?method=get_service_endpoint&scope=&next_cursor=0'
@@ -34,24 +43,39 @@ async def aiohttp_get(url, paginated=False):
 
     suffix = url.split('method=')[1]
 
+    http_retry_options = ExponentialRetry(attempts=2)
+    # http_retry_options = ListRetry(timeouts=[10.0, 20.0, 30.0, 40.0])
+    client_timeout = aiohttp.ClientTimeout(total=1*15, connect=None,
+                                           sock_connect=None, sock_read=None)
+    conn_try, connection_attempts = 1, 3
+
     print('aiohttp_get - started', f' - {time.ctime()}')
     try:
-        async with aiohttp.ClientSession() as session:
+        async with RetryClient(retry_options=http_retry_options, timeout=client_timeout) as session:
             if paginated:
                 count, cursor = 1, 0
                 while count != 0:
-                    async with session.get(url + f'&scope=&next_cursor={cursor}', ssl=sslcontext) as resp:
-                        content = await resp.text()
-                        parsed = xml.dom.minidom.parseString(content)
-                        write_file(suffix, content)
-                        count = int(parsed.getElementsByTagName('count')[0].childNodes[0].data)
-                        links = parsed.getElementsByTagName('link')
-                        for le in links:
-                            if le.getAttribute('rel') == 'next':
-                                href = le.getAttribute('href')
-                                for e in href.split('&'):
-                                    if 'next_cursor' in e:
-                                        cursor = e.split('=')[1]
+                    while conn_try <= connection_attempts:
+                        try:
+                            async with session.get(url + f'&scope=&next_cursor={cursor}',
+                                                ssl=sslcontext) as resp:
+                                content = await resp.text()
+                                parsed = xml.dom.minidom.parseString(content)
+                                write_file(suffix, content)
+                                count = int(parsed.getElementsByTagName('count')[0].childNodes[0].data)
+                                links = parsed.getElementsByTagName('link')
+                                for le in links:
+                                    if le.getAttribute('rel') == 'next':
+                                        href = le.getAttribute('href')
+                                        for e in href.split('&'):
+                                            if 'next_cursor' in e:
+                                                cursor = e.split('=')[1]
+                        except asyncio.TimeoutError as e:
+                            print('connection try - ', conn_try)
+                        conn_try += 1
+                    else:
+                        print('connection exhausted')
+                        break
                 print(f'file-{suffix} written', f' - {time.ctime()}')
             else:
                 async with session.get(url, ssl=sslcontext) as resp:
@@ -83,7 +107,7 @@ if __name__ == '__main__':
         ))
         print("End", f'- {time.ctime()}')
         print("Sequential", f'- {time.ctime()}')
-        loop.run_until_complete(sequential())
+        loop.run_until_complete(sequential(True))
         print("End", f'- {time.ctime()}')
 
     finally:

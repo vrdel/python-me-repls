68405f8cce86471e6991ce34b8e2978ad2ae4af4
Daniel Vrcic <daniel.vrcic@gmail.com>
2021-01-14 16:48:18 +0100

    SIGTERM handle

--- basic/evloop-run-exit.py
+++ basic/evloop-run-exit.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
 
 import asyncio
+import functools
+import os
+import signal
 
 async def f(delay):
     await asyncio.sleep(delay)
+
+def signal_handler(name):
+    print(f'signal_handler({name})')
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     t1 = loop.create_task(f(60))
     t2 = loop.create_task(f(60))
+    loop.add_signal_handler(signal.SIGTERM, functools.partial(signal_handler, name='SIGTERM'),)
     try:
         loop.run_until_complete(asyncio.gather(
             t1, t2

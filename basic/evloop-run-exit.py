2c7cf7ee2c98bfc49836209e25fcbc86cadb5018
Daniel Vrcic <daniel.vrcic@gmail.com>
2021-01-14 11:20:43 +0100

    example of destroy pending task

---
+++ basic/evloop-run-exit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import asyncio
+
+async def f(delay):
+    await asyncio.sleep(delay)
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    t1 = loop.create_task(f(1))
+    t2 = loop.create_task(f(2))
+    loop.run_until_complete(t1)
+    loop.close()

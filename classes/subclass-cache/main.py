02c85d21b35cab3f04e659f600c5ab192f640d18
Daniel Vrcic <daniel.vrcic@gmail.com>
2021-11-05 11:30:22 +0100

    subclassing does not work because of overloaded __new__ in A

--- classes/subclass-cache/main.py
+++ classes/subclass-cache/main.py
@@ -5,17 +5,11 @@ from b import B
 
 def main():
     ac = A()
-    print(ac.value)
     ac.value = 'changed_classA_value'
     bc = B()
 
     print(bc.value)
-    ac.value = 'changed_secondtime_classA_value'
-    print(bc.value)
-    bc.cvalue = 'changed_classB_value'
-    print(ac.value)
-    print(bc.cvalue)
-
+    print(type(bc)) # class A here
 
 if __name__ == '__main__':
     main()

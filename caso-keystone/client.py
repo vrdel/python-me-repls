a97caef062888d0099fcb49ada678fe4c573d0fb
Daniel Vrcic <daniel.vrcic@gmail.com>
2025-03-10 16:03:25 +0100

    refactored var names without admin_ prefix and handle connection failures

--- caso-keystone/client.py
+++ caso-keystone/client.py
@@ -9,49 +9,61 @@ from log import Logger
 from keystoneauth1.identity import v3
 from keystoneclient.v3 import client
 from keystoneauth1 import session
+from keystoneauth1 import exceptions
 
 
 logger = Logger('test-keystone-client').get()
 
 
 class IdentityClient(object):
-    def __init__(self, logger, admin_user, admin_password, admin_project,
-                 admin_project_id, url, member_role):
+    def __init__(self, logger, user, password, project,
+                 project_id, project_domain_id, user_domain_id, url,
+                 member_role):
         self.logger = logger
-        self.admin_user = admin_user
-        self.admin_password = admin_password
-        self.admin_project = admin_project
-        self.admin_project_id = admin_project_id
+        self.user = user
+        self.password = password
+        self.project = project
+        self.project_id = project_id
         self.url = url
         self.member_role = member_role
         self.project_id = None
+        self.project_domain_id = project_domain_id
+        self.user_domain_id = user_domain_id
         self.setup_client()
 
     def get_role(self, name):
         found = filter(lambda r: r.name == name,
-                       self.admin_client.roles.list())
+                       self.client.roles.list())
         return found[0] if found else None
 
     def setup_client(self):
-        auth = v3.Password(auth_url=self.url, username=self.admin_user,
-                           password=self.admin_password,
-                           user_domain_id='default',
-                           project_id=self.admin_project_id,
-                           project_domain_id='Default',
-                           project_name=self.admin_project)
+        auth = v3.Password(auth_url=self.url, username=self.user,
+                           password=self.password,
+                           user_domain_id=self.user_domain_id,
+                           project_id=self.project_id,
+                           project_domain_id=self.project_domain_id,
+                           project_name=self.project)
         sess = session.Session(auth=auth)
-        self.admin_client = client.Client(session=sess, interface='public')
+        self.client = client.Client(session=sess, interface='public')
         self.role = self.get_role(self.member_role)
 
 
 def main():
     confopts = parse_config()
-    keystone = IdentityClient(logger, confopts['openstack']['username'],
-                              confopts['openstack']['password'],
-                              confopts['openstack']['project_name'],
-                              confopts['openstack']['project_id'],
-                              confopts['openstack']['url'],
-                              confopts['openstack']['member_role'])
+    try:
+        keystone = IdentityClient(logger, confopts['openstack']['username'],
+                                  confopts['openstack']['password'],
+                                  confopts['openstack']['project_name'],
+                                  confopts['openstack']['project_id'],
+                                  confopts['openstack']['project_domain_id'],
+                                  confopts['openstack']['user_domain_id'],
+                                  confopts['openstack']['url'],
+                                  confopts['openstack']['member_role'])
+
+    except exceptions.connection.ConnectFailure as exc:
+        logger.error(repr(exc))
+    except Exception as exc:
+        logger.error(f'{exc.url}({exc.method}) {repr(exc)}')
 
 
 if __name__ == '__main__':
